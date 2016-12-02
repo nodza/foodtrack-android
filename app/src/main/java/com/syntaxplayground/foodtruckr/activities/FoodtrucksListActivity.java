37ba0c80d7c1aaeb48eb205158f8ef03405b3df0
nodza <noel.hwande@gmail.com>
2016-12-01 20:23:00 -0600

    Refactored data service

--- app/src/main/java/com/syntaxplayground/foodtruckr/activities/FoodtrucksListActivity.java
+++ app/src/main/java/com/syntaxplayground/foodtruckr/activities/FoodtrucksListActivity.java
@@ -14,6 +14,7 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.syntaxplayground.foodtruckr.R;
 import com.syntaxplayground.foodtruckr.adapter.FoodtruckAdapter;
+import com.syntaxplayground.foodtruckr.data.DataService;
 import com.syntaxplayground.foodtruckr.model.Foodtruck;
 import com.syntaxplayground.foodtruckr.view.ItemDecorator;
 
@@ -27,63 +28,38 @@ public class FoodtrucksListActivity extends AppCompatActivity {
 
     // Variables
     private FoodtruckAdapter adapter;
+    private ArrayList<Foodtruck> foodtrucks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodtrucks_list);
 
-        String url = "http://10.0.2.2:3005/api/v1/foodtruck";
-        final ArrayList<Foodtruck> foodtruckList = new ArrayList<>();
-
-        final JsonArrayRequest getTrucks = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+        TrucksDownloaded listener = new TrucksDownloaded() {
             @Override
-            public void onResponse(JSONArray response) {
-                System.out.println(response.toString());
-
-                try {
-                    JSONArray foodtrucks = response;
-                    for (int x = 0; x < foodtrucks.length(); x++) {
-                        JSONObject foodtruck = foodtrucks.getJSONObject(x);
-
-                        String id = foodtruck.getString("_id");
-                        String name = foodtruck.getString("name");
-                        String foodType = foodtruck.getString("foodType");
-                        Double avgCost = foodtruck.getDouble("avgCost");
-
-                        JSONObject geometry = foodtruck.getJSONObject("geometry");
-                        JSONObject coordinates = geometry.getJSONObject("coordinates");
-
-                        Double latitude = coordinates.getDouble("lat");
-                        Double longitude = coordinates.getDouble("lng");
-
-                        Foodtruck newFoodtruck = new Foodtruck(longitude, id, name, foodType, avgCost, latitude);
-                        foodtruckList.add(newFoodtruck);
-
-                    }
-                } catch (JSONException e) {
-                    Log.v("JSON", "EXC" + e.getLocalizedMessage());
+            public void success(Boolean success) {
+                if (success) {
+                    setUpRecycler();
                 }
-
-                System.out.println("My favorite foodtruck is " + foodtruckList.get(1).getName());
-
-                RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_foodtruck);
-                recyclerView.setHasFixedSize(true);
-                adapter = new FoodtruckAdapter(foodtruckList);
-                recyclerView.setAdapter(adapter);
-                LinearLayoutManager layoutManager = new LinearLayoutManager(getBaseContext());
-                layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-                recyclerView.setLayoutManager(layoutManager);
-                recyclerView.addItemDecoration(new ItemDecorator(0, 0, 0, 10));
-
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.v("API", "Err" + error.getLocalizedMessage());
-            }
-        });
+        };
 
-        Volley.newRequestQueue(this).add(getTrucks);
+        setUpRecycler();
+        foodtrucks = DataService.getInstance().downloadAllFoodtrucks(this, listener);
+    }
+
+    private void setUpRecycler() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_foodtruck);
+        recyclerView.setHasFixedSize(true);
+        adapter = new FoodtruckAdapter(foodtrucks);
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getBaseContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new ItemDecorator(0, 0, 0, 10));
+    }
+
+    public interface TrucksDownloaded {
+        void success(Boolean success);
     }
 }
---
+++ app/src/main/java/com/syntaxplayground/foodtruckr/data/DataService.java
@@ -0,0 +1,84 @@
+package com.syntaxplayground.foodtruckr.data;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.syntaxplayground.foodtruckr.activities.FoodtrucksListActivity;
+import com.syntaxplayground.foodtruckr.model.Foodtruck;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nodza on 12/1/16.
+ */
+
+public class DataService {
+
+    private static DataService instance = new DataService();
+
+    public static DataService getInstance() {
+        return instance;
+    }
+
+    private DataService() {
+
+    }
+
+    // Request all foodtrucks
+    public ArrayList<Foodtruck> downloadAllFoodtrucks(Context context, final FoodtrucksListActivity.TrucksDownloaded listener) {
+
+        String url = "http://10.0.2.2:3005/api/v1/foodtruck";
+        final ArrayList<Foodtruck> foodtruckList = new ArrayList<>();
+
+        final JsonArrayRequest getTrucks = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                System.out.println(response.toString());
+
+                try {
+                    JSONArray foodtrucks = response;
+                    for (int x = 0; x < foodtrucks.length(); x++) {
+                        JSONObject foodtruck = foodtrucks.getJSONObject(x);
+
+                        String id = foodtruck.getString("_id");
+                        String name = foodtruck.getString("name");
+                        String foodType = foodtruck.getString("foodType");
+                        Double avgCost = foodtruck.getDouble("avgCost");
+
+                        JSONObject geometry = foodtruck.getJSONObject("geometry");
+                        JSONObject coordinates = geometry.getJSONObject("coordinates");
+
+                        Double latitude = coordinates.getDouble("lat");
+                        Double longitude = coordinates.getDouble("lng");
+
+                        Foodtruck newFoodtruck = new Foodtruck(longitude, id, name, foodType, avgCost, latitude);
+                        foodtruckList.add(newFoodtruck);
+
+                    }
+                } catch (JSONException e) {
+                    Log.v("JSON", "EXC" + e.getLocalizedMessage());
+                }
+
+                listener.success(true);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("API", "Err" + error.getLocalizedMessage());
+            }
+        });
+
+        Volley.newRequestQueue(context).add(getTrucks);
+        return foodtruckList;
+    }
+}
